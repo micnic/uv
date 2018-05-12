0c016597378e1e83e3600032eb26de01d3a46f71
micnic <micnic90@gmail.com>
2018-05-12 13:19:39 +0300

    update bench.js and fix test command in package.json

--- bench.js
+++ bench.js
@@ -2,51 +2,42 @@
 
 const Benchmark = require('benchmark');
 
+const https = require('https');
+
 const uv = require('uv');
 const isValidUTF8 = require('utf-8-validate');
 const isUtf8 = require('isutf8');
 
-const suite = new Benchmark.Suite;
+https.get('https://ro.wikipedia.org/wiki/Pagina_principal%C4%83', (response) => {
 
-const validBuffer = Buffer.from('abcdefghijklmnopqrstuvwxyz');
-const invalidBuffer = Buffer.from(validBuffer);
-const randomBuffer = Buffer.from(Array.from(Array(26).map(() => Math.floor(Math.random() * 255))));
+	console.log('Loading Romanian Wikipedia main page ...');
 
-// Invalidate UTF-8 data
-invalidBuffer[Math.floor(validBuffer.length / 2)] = 255;
+	let content = Buffer.alloc(0);
 
-// add tests
-suite
-.add('uv valid buffer', () => {
-	uv(validBuffer);
-})
-.add('uv invalid buffer', () => {
-	uv(invalidBuffer);
-})
-.add('uv random buffer', () => {
-	uv(randomBuffer);
-})
-.add('utf-8-validate valid buffer', () => {
-	isValidUTF8(validBuffer);
-})
-.add('utf-8-validate invalid buffer', () => {
-	isValidUTF8(invalidBuffer);
-})
-.add('utf-8-validate random buffer', () => {
-	isValidUTF8(randomBuffer);
-})
-.add('isutf8 valid buffer', () => {
-	isUtf8(validBuffer);
-})
-.add('isutf8 invalid buffer', () => {
-	isUtf8(invalidBuffer);
-})
-.add('isutf8 random buffer', () => {
-	isUtf8(randomBuffer);
-})
-// add listeners
-.on('cycle', (event) => {
-  console.log(String(event.target));
-})
-// run async
-.run({ 'async': true });
+	response.on('data', (data) => {
+		content = Buffer.concat([content, data]);
+	}).on('end', () => {
+
+		const buffer = Buffer.from(content);
+
+		const suite = new Benchmark.Suite;
+
+		// add tests
+		suite
+		.add('uv valid buffer', () => {
+			uv(buffer);
+		})
+		.add('utf-8-validate valid buffer', () => {
+			isValidUTF8(buffer);
+		})
+		.add('isutf8 valid buffer', () => {
+			isUtf8(buffer);
+		})
+		// add listeners
+		.on('cycle', (event) => {
+			console.log(String(event.target));
+		})
+		// run async
+		.run({ 'async': true });
+	});
+});
